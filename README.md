7ffe7ff85838a6781093f506f6fac9b0e4903cc2
frendit-xyz <130228825+frendit-xyz@users.noreply.github.com>
2023-06-25 12:25:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# track-daily-plans
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-react-tailwind-xstate-counter-csgarp)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex flex-col h-screen w-full bg-slate-50">
+      <div className="p-3 border-b border-slate-300 w-full flex"></div>
+    </div>
+  );
+}
+export default App;
